86500aa581bc4c85267439d3925c7669100ba440
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-06 15:24:12 +0600

    adding an object

--- app/function.js
+++ app/function.js
@@ -3,4 +3,8 @@ const ageCal = (name, age) => {
     return` Hi ${name}, you are ${age} years old `
 }
 
+const name = " Asikul Islam "
+
+const fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Pineapple', 'Strawberry', ]
+
 module.exports = ageCal;
