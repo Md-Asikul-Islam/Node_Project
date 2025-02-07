e0dce225ac48786f57b38be61ded2974d3f7f061
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-07 22:35:34 +0600

    make a server

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const {ageCal, devs,fruits, name} = require("./app/function")
+const http = require("http"); 
+const url = require("url");
 
-console.log(ageCal(" Mehedi Hassan Himel", 30))
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true); 
+  const query = parsedUrl.query;
+
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ query }));
+});
+server.listen(4040);
