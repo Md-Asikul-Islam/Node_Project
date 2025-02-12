a6bad9ecc8120dd4c7274193f61afa4a06ba2f69
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-02-12 10:24:49 +0600

    adding port

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 const colors = require("colors");
+const dotenv = require("dotenv").config()
 
-
+const PORT = process.env.SERVER_PORT
 
 // create a server 
-http.createServer().listen(5050, ()=> {
-    console.log(` The server is running on port5050`.bgGreen.black)
+http.createServer().listen(PORT, ()=> {
+    console.log(` The server is running on port${PORT}`.bgGreen.black)
 })
